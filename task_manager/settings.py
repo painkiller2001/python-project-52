1c22cb412c4b05fad1d9ce509e62ad019e17a818
painkiller2001 <valeryfiadarenkou@EPBYMINW0510.localdomain>
2026-08-16 19:14:13 +0300

    First view, template created

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3t#h#sn&b#nslnrfla0=tmq55h6!21$bk++fz7%$3o6$ezg@v$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
