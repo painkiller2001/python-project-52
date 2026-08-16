0eebdbeb6e782754b5608a2e7dfbf2bf0b494b0d
painkiller2001 <fsvfsvfsva@yandex.by>
2026-08-16 19:14:13 +0300

    First view, template created

---
+++ task_manager/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'index.html',
+            context={
+                'name': 'Valerik'
+            }
+        )
